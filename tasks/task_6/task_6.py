b40db2abbc9d4085675bcd3e1b898574bb03a4ff
Orangeboy115 <puneetpowar10@gmail.com>
2024-05-28 14:27:53 -0400

    Finished Task 6
    Completed functionality, that allows use to upload course content, and make a query on the number of questions the user wants and the topic the question should be on.

--- tasks/task_6/task_6.py
+++ tasks/task_6/task_6.py
@@ -1,6 +1,7 @@
 import sys
 import os
 import streamlit as st
+
 sys.path.append(os.path.abspath('../../'))
 from tasks.task_3.task_3 import DocumentProcessor
 from tasks.task_4.task_4 import EmbeddingClient
@@ -45,41 +46,47 @@ if __name__ == "__main__":
     # Configuration for EmbeddingClient
     embed_config = {
         "model_name": "textembedding-gecko@003",
-        "project": "YOUR PROJECT ID HERE",
+        "project": "quizify-424619",
         "location": "us-central1"
     }
-    
-    screen = st.empty() # Screen 1, ingest documents
+
+    screen = st.empty()  # Screen 1, ingest documents
     with screen.container():
         st.header("Quizzify")
-        ####### YOUR CODE HERE #######
         # 1) Initalize DocumentProcessor and Ingest Documents from Task 3
+        processor = DocumentProcessor()
+        processor.ingest_documents()
         # 2) Initalize the EmbeddingClient from Task 4 with embed config
+        embed_client = EmbeddingClient(**embed_config)
         # 3) Initialize the ChromaCollectionCreator from Task 5
-        ####### YOUR CODE HERE #######
+        chroma_creator = ChromaCollectionCreator(processor, embed_client)
 
         with st.form("Load Data to Chroma"):
             st.subheader("Quiz Builder")
-            st.write("Select PDFs for Ingestion, the topic for the quiz, and click Generate!")
-            
+            st.write(
+                "Select PDFs for Ingestion, the topic for the quiz, and click Generate!")
+
             ####### YOUR CODE HERE #######
             # 4) Use streamlit widgets to capture the user's input
-            # 4) for the quiz topic and the desired number of questions
+            topic_input = st.text_input("Topic for Generative Quiz", placeholder="Enter the topic of the document")
+            # 5) for the quiz topic and the desired number of questions
+            slider_val = st.slider("Number of Questions", min_value=1, max_value=10, value=5)
             ####### YOUR CODE HERE #######
-            
+
             document = None
-            
+
             submitted = st.form_submit_button("Generate a Quiz!")
             if submitted:
+                print('hello')
                 ####### YOUR CODE HERE #######
                 # 5) Use the create_chroma_collection() method to create a Chroma collection from the processed documents
                 ####### YOUR CODE HERE #######
-                    
+                chroma_creator.create_chroma_collection()
                 # Uncomment the following lines to test the query_chroma_collection() method
-                # document = chroma_creator.query_chroma_collection(topic_input) 
-                
+                document = chroma_creator.query_chroma_collection(topic_input)
+
     if document:
-        screen.empty() # Screen 2
+        screen.empty()  # Screen 2
         with st.container():
             st.header("Query Chroma for Topic, top Document: ")
             st.write(document)
